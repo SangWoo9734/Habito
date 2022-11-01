b7e612ec78f5c900a1d61183c8ed56ca2d19c502
Parksangwoo <psu8430@gmail.com>
2022-11-02 00:21:57 +0900

    Setting: color variable 설정

---
+++ src/assets/styleVariables.ts
@@ -0,0 +1,8 @@
+export const FIRST_COLOR = "#3277FE";
+export const SECOND_COLOR = "#6FA0FF";
+export const THIRD_COLOR = "#B4CDFF";
+
+export const DELETE = "#FF6060";
+
+export const MAIN_GRAY = "#DFDFDF";
+export const SUB_GRAY = "#EEEEEE";
